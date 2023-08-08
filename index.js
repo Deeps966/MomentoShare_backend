52aa639d5173bef8b474bf24b4fe19c254e15a19
Deep Shah <shahdeep966@gmail.com>
2023-08-08 22:38:23 +0530

    setup with postgresql database and first route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const pool = require('pg')
 
 // Middleware
 app.use(cors())
@@ -8,6 +9,13 @@ app.use(express.json())
 
 // Server Port
 const PORT = 5000
+
+// ----------ROUTES----------
+app.get("/users", async (req, res) => {
+  res.json({
+    data: "Hello World"
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`)
